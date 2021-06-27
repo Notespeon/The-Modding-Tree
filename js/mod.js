47264d78b16a4d48e51d7608c2aebb3c833f1f9c
Richard Engelke <a1687231@student.adelaide.edu.au>
2021-06-27 18:18:52 +0930

    Minor Fix

--- js/mod.js
+++ js/mod.js
@@ -56,7 +56,7 @@ var displayThings = [
 
 // Determines when the game "ends"
 function isEndgame() {
-	return player.points.gte(new Decimal("3601"))
+	return player.points.gte(new Decimal("3600"))
 }
 
 
